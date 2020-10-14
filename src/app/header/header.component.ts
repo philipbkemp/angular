97fba27b4e3fbb30bcfd76cc6aac9d9adcb18580
Phil Kemp <boondock.dragon@gmail.com>
2020-10-14 13:50:24 +0200

    85: Adding Navigation with Event Binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component,EventEmitter, Output } from "@angular/core";
 
 @Component({
 	selector: "app-header",
@@ -6,4 +6,10 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
 	
+	@Output modeSelected = new EventEmitter<string>();
+
+	onSelect(mode:string) {
+		this.modeSelected.emit(mode);
+	}
+
 }
