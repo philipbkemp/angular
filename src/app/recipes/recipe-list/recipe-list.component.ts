2ef7d552aa3ceacc8676be2bb2e332c2acd74fd5
Phil Kemp <boondock.dragon@gmail.com>
2020-10-20 10:42:50 +0200

    117: Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
 	selector: 'app-recipe-list',
 	templateUrl: './recipe-list.component.html',
 	styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
 	@Output() showRecipe = new EventEmitter<Recipe>();
 
-	recipes: Recipe[] = [
-		new Recipe("Beans on toast","Proper student grub","https://aucdn.ar-cdn.com/recipes/port320/ed970612-f30b-414a-a93a-322ba214e7c1.jpg"),
-		new Recipe("Carbonara","Hearty pasta","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg"),
-		new Recipe("Egg fried rice","Eggs, Rice, Fried","https://i0.wp.com/seonkyounglongest.com/wp-content/uploads/2020/02/Egg-Fried-Rice-2.jpg?fit=1300%2C867&ssl=1")
-	];
+	recipes: Recipe[];
+
+	constructor(
+		private rcpService: RecipeService
+		) {}
+
+	ngOnInit() {
+		this.recipes = this.rcpService.getRecipes();
+	}
 
 	recipeSelected(rcp:Recipe) {
 		this.showRecipe.emit(rcp);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-	
+
+	private recipes: Recipe[] = [
+		new Recipe("Beans on toast","Proper student grub","https://aucdn.ar-cdn.com/recipes/port320/ed970612-f30b-414a-a93a-322ba214e7c1.jpg"),
+		new Recipe("Carbonara","Hearty pasta","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg"),
+		new Recipe("Egg fried rice","Eggs, Rice, Fried","https://i0.wp.com/seonkyounglongest.com/wp-content/uploads/2020/02/Egg-Fried-Rice-2.jpg?fit=1300%2C867&ssl=1")
+	];
+
+	getRecipes() {
+		return this.recipes.slice();
+	}
+
 }
