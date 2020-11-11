3c001776135a888f351beef50386ef133eb21bd5
Phil Kemp <boondock.dragon@gmail.com>
2020-11-11 09:26:03 +0100

    156: Adding Navigation to the App

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,9 @@
-import { Component,EventEmitter, Output } from "@angular/core";
+import { Component, } from "@angular/core";
 
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-	
-	@Output() modeSelected = new EventEmitter<string>();
-
-	onSelect(mode:string) {
-		this.modeSelected.emit(mode);
-	}
 
 }
