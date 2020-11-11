a58c25d71c9c2bc879727f07f2bdafa0ffdec3a7
Phil Kemp <boondock.dragon@gmail.com>
2020-11-11 09:24:04 +0100

    155: Setting Up Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
+import { AppRoutingModule} from "./app-routing.module";
 
 import { DropdownDirective } from "./shared/dropdown.directive";
 
@@ -29,6 +30,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [
     ShoppingListService,
