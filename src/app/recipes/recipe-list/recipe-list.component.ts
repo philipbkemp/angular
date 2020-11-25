547d38a6f5ee55a73a0959ff59cf62fb04f1314d
Phil Kemp <boondock.dragon@gmail.com>
2020-11-25 11:36:49 +0100

    Fixed compile bug

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
-import { Subscription } from "rxjs/Subscription";
+import { Subscription } from "rxjs";
 
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
