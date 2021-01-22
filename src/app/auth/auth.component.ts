4eb4bc9809ef3878984a26888f12efccdee87734
Phil Kemp <boondock.dragon@gmail.com>
2021-01-22 11:40:59 +0100

    289: Handling Form Input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
 	selector: "app-auth",
-	templateUrl: "./auth.component.html"
+	templateUrl: "./auth.component.html",
+	styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
 
@@ -10,6 +12,23 @@ export class AuthComponent {
 
 	onSwitchMode() {
 		this.modeLogin = ! this.modeLogin;
+	}
+
+	onSubmit(form: NgForm) {
+
+		let formOk = true;
+
+		if ( form.value.password2 ) {
+			formOk = (form.value.password === form.value.password2);
+			if ( ! formOk ) {
+				form.controls["password2"].setErrors({"invalid":true});
+			}
+		}
+
+		if ( formOk ) {
+			console.log(form.value);
+			form.reset();
+		}
 	}
 
 }
